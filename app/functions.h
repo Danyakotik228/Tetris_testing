6fdadb8060bf7ccbce1d6f6bf2d3a4dd7e4f71dc
zdoroven <45197696+zdoroven@users.noreply.github.com>
2019-05-30 19:45:28 +0300

    Update functions.h

--- app/functions.h
+++ app/functions.h
@@ -1,7 +1,31 @@
 #ifndef MYFUNC_H
 #define MYFUNC_H
+
+#include <iostream>
+#include <QFile>
+#include <string>
+#include <cstdio>
+#include <fstream>
+#include <QDebug>
+#include <cstdlib>
+#include <algorithm>
+using namespace std;
+
+bool executeTest(QString output, QString expected)
+{
+    if (output == expected)
+        return true;
+    else
+        return false;
+}
+
 struct Point
 {int x,y;} a[4], b[4];
+
+struct S{
+      int num;
+      QString sss;
+    };
 
 void cleararray(int field[20][10]);
 int calculate(int sum, int k);
