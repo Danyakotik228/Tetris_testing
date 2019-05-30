9fe3e7b6b4e297264a2c491aeaa2112b3832bca7
zdoroven <45197696+zdoroven@users.noreply.github.com>
2019-05-30 19:52:02 +0300

    Update sort.h

--- tests/sort.h
+++ tests/sort.h
@@ -4,7 +4,7 @@
 #include <gtest/gtest.h>
 #include "functions.h"
 
-TEST(sort, emptyFile)
+TEST(sort, empty)
 {
     QString inputPath = "./tests/input/input1.txt";
     QFile input(inputPath);
@@ -42,4 +42,79 @@ TEST(sort, emptyFile)
         FAIL();
 }
 
+TEST(sort, repeatnames)
+{
+    QString inputPath = "./tests/input/input2.txt";
+    QFile input(inputPath);
+
+    QString outputPath = "./tests/output/output2.txt";
+    QFile output(outputPath);
+
+    sorting(inputPath, outputPath);
+
+    QFile expected("./expected/expected2.txt");
+
+    QString outputString;
+    QString expectedString;
+
+    if (output.open(QIODevice::ReadOnly)){
+        while(!output.atEnd()){
+            outputString = output.readAll();
+        }
+        output.close();
+    }
+
+    if (expected.open(QIODevice::ReadOnly)){
+        while(!expected.atEnd()){
+            expectedString = expected.readAll();
+        }
+        expected.close();
+    }
+
+    cout << "Output file: " << outputString.toStdString() << endl;
+    cout << "Expected file: "<< expectedString.toStdString() << endl;
+
+    if (executeTest(outputString, expectedString))
+        SUCCEED();
+    else
+        FAIL();
+}
+
+TEST(sort, simpleworking)
+{
+    QString inputPath = "./tests/input/input3.txt";
+    QFile input(inputPath);
+
+    QString outputPath = "./tests/output/output3.txt";
+    QFile output(outputPath);
+
+    sorting(inputPath, outputPath);
+
+    QFile expected("./expected/expected3.txt");
+
+    QString outputString;
+    QString expectedString;
+
+    if (output.open(QIODevice::ReadOnly)){
+        while(!output.atEnd()){
+            outputString = output.readAll();
+        }
+        output.close();
+    }
+
+    if (expected.open(QIODevice::ReadOnly)){
+        while(!expected.atEnd()){
+            expectedString = expected.readAll();
+        }
+        expected.close();
+    }
+
+    cout << "Output file: " << outputString.toStdString() << endl;
+    cout << "Expected file: "<< expectedString.toStdString() << endl;
+
+    if (executeTest(outputString, expectedString))
+        SUCCEED();
+    else
+        FAIL();
+}
 #endif // SORT_H
