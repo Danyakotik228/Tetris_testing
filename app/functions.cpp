3600d50722c43c5bd770f903e94da5cbf76578e8
zdoroven <45197696+zdoroven@users.noreply.github.com>
2019-05-31 14:37:36 +0300

    Update functions.cpp

--- app/functions.cpp
+++ app/functions.cpp
@@ -1,6 +1,6 @@
 #include "functions.h"
 #include <QStringList>
-    point a[4], b[4];
+
 bool executeTest(QString output, QString expected)
 {
     if (output == expected)
@@ -22,16 +22,6 @@ void cleararray(int field[20][10])
 int calculate(int sum, int k){
     sum += k;
     return sum;
-}
-
-int check(int field[20][10])
-{
-    int i;
-   for (i=0;i<4;i++)
-      if (a[i].x<0 || a[i].x>=10 || a[i].y>=20) return 0;
-      else if (field[a[i].y][a[i].x]) return 0;
-
-   return 1;
 }
 
 void sorting(QString Path, QString OutputPath)
