e99d0490d0b9030bbca3ee5c3a5f33641de30b17
zdoroven <45197696+zdoroven@users.noreply.github.com>
2019-05-31 14:37:59 +0300

    Update functions.h

--- app/functions.h
+++ app/functions.h
@@ -13,11 +13,6 @@ using namespace std;
 
 bool executeTest(QString output, QString expected);
 
-struct Point
-{
-      int x,y;
-};
-
 struct S{
       int num;
       QString sss;
@@ -25,5 +20,4 @@ struct S{
 
 void cleararray(int field[20][10]);
 int calculate(int sum, int k);
-int check(int field[20][10]);
 #endif // FUNCTIONS_H
