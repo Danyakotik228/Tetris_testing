133ff925c77cf7351900bb9fa044f793ed748652
zdoroven <45197696+zdoroven@users.noreply.github.com>
2019-05-31 14:28:12 +0300

    Update functions.cpp

--- app/functions.cpp
+++ app/functions.cpp
@@ -1,6 +1,14 @@
 #include "functions.h"
 #include <QStringList>
 
+bool executeTest(QString output, QString expected)
+{
+    if (output == expected)
+        return true;
+    else
+        return false;
+}
+
 void cleararray(int field[20][10])
 {
     int i, j;
