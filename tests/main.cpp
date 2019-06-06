77476ce37e8698de41cb72d0972331688fbcdc4a
zdoroven <45197696+zdoroven@users.noreply.github.com>
2019-06-06 13:38:06 +0300

    Update main.cpp

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,10 +1,6 @@
 #include "cleararray.h"
 #include "calculate.h"
-<<<<<<< HEAD
 #include "check.h"
-=======
->>>>>>> cdc9855559533051b4fb370810864d4ba6cb9b1e
-
 #include <gtest/gtest.h>
 
 int main(int argc, char *argv[])
