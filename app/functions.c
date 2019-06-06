2bedccdb5d950026ee888960bf0855b163735f88
zdoroven <45197696+zdoroven@users.noreply.github.com>
2019-06-06 13:42:54 +0300

    Update functions.c

--- app/functions.c
+++ app/functions.c
@@ -3,7 +3,8 @@
 
 void cleararray(int field[20][10])
 {
-    for (int i = 0; i < 20; i++) {
+    int i;
+    for (i = 0; i < 20; i++) {
         for(int j = 0; j < 10; j++){
             field[i][j] = 0;
         }
@@ -17,7 +18,8 @@ int calculate(int sum, int k){
 
 int check(int field[20][10])
 {
-   for (int i=0;i<4;i++)
+    int i;
+   for (i=0;i<4;i++)
       if (a[i].x<0 || a[i].x>=10 || a[i].y>=20) return 0;
       else if (field[a[i].y][a[i].x]) return 0;
 
