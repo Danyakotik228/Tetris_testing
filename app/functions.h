006006f0d23d7526e78a6d765e07256e01f7d30e
zdoroven <45197696+zdoroven@users.noreply.github.com>
2019-06-06 13:39:10 +0300

    Update functions.h

--- app/functions.h
+++ app/functions.h
@@ -1,13 +1,7 @@
-<<<<<<< HEAD
-#ifndef MYFUNC_H
-#define MYFUNC_H
-struct Point
-{int x,y;} a[4], b[4];
-
-=======
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
->>>>>>> cdc9855559533051b4fb370810864d4ba6cb9b1e
+struct Point
+{int x,y;} a[4], b[4];
 void cleararray(int field[20][10]);
 int calculate(int sum, int k);
 int check(int field[20][10]);
